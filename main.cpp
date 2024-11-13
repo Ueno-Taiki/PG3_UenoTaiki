7d45ab1a87a5e6237c95d9ba68fb325c85d96404
上野太己 <k023g0051@g.neec.ac.jp>
2024-11-13 22:12:29 +0900

    02_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,15 +1,59 @@
 #include <stdio.h>
-#include "IShape.h"
+#include <stdlib.h>
+
+//基底クラス: Animal
+typedef struct {
+    void (*speak)(void);
+} Animal;
+
+//Dogクラス
+typedef struct {
+    Animal base;
+} Dog;
+
+void dogSpeak() {
+    printf("Wan!\n");
+}
+
+Dog* newDog() {
+    Dog* dog = (Dog*)malloc(sizeof(Dog));
+    dog->base.speak = dogSpeak;
+    return dog;
+}
+
+//Catクラス
+typedef struct {
+    Animal base;
+} Cat;
+
+void catSpeak() {
+    printf("Nya-!\n");
+}
+
+Cat* newCat() {
+    Cat* cat = (Cat*)malloc(sizeof(Cat));
+    cat->base.speak = catSpeak;
+    return cat;
+}
+
+//Animalインスタンスのspeakメソッドを呼び出す関数
+void makeAnimalSpeak(Animal* animal) {
+    animal->speak();
+}
 
 int main() {
-	IShape* iShapes[3];
 
-	for (int i = 0; i < 2; i++) {
-		if (i < 1) 
-			iShapes[i] = new Circle;
-		else 
-			iShapes[i] = new Rectangle;
-	}
+    //DogとCatのインスタンスを生成
+    Dog* dog = newDog();
+    Cat* cat = newCat();
+
+    //ポリモーフィズムによる動的な振る舞い
+    makeAnimalSpeak((Animal*)dog);
+    makeAnimalSpeak((Animal*)cat);
+
+    //メモリ解放
+    free(dog);
+    free(cat);
 
 	return 0;
 }
