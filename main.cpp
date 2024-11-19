5e1aaf79cceb4e7e8080bc7e8b804a4863c13443
上野太己 <k023g0051@g.neec.ac.jp>
2024-11-19 12:57:03 +0900

    03_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,59 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <list>
 
-//基底クラス: Animal
-typedef struct {
-    void (*speak)(void);
-} Animal;
-
-//Dogクラス
-typedef struct {
-    Animal base;
-} Dog;
-
-void dogSpeak() {
-    printf("Wan!\n");
-}
-
-Dog* newDog() {
-    Dog* dog = (Dog*)malloc(sizeof(Dog));
-    dog->base.speak = dogSpeak;
-    return dog;
-}
-
-//Catクラス
-typedef struct {
-    Animal base;
-} Cat;
-
-void catSpeak() {
-    printf("Nya-!\n");
-}
-
-Cat* newCat() {
-    Cat* cat = (Cat*)malloc(sizeof(Cat));
-    cat->base.speak = catSpeak;
-    return cat;
-}
-
-//Animalインスタンスのspeakメソッドを呼び出す関数
-void makeAnimalSpeak(Animal* animal) {
-    animal->speak();
-}
+using namespace std;
 
 int main() {
 
-    //DogとCatのインスタンスを生成
-    Dog* dog = newDog();
-    Cat* cat = newCat();
+	list<const char*> station{ "Nishi-Nippori","Takanawa Gateway","Shimbashi" };  //西日暮里駅、高輪ゲートウェイ、新橋で初期化
 
-    //ポリモーフィズムによる動的な振る舞い
-    makeAnimalSpeak((Animal*)dog);
-    makeAnimalSpeak((Animal*)cat);
-
-    //メモリ解放
-    free(dog);
-    free(cat);
+	for (auto itr = station.begin();
+		itr != station.end();
+		itr++)
+	{
+		cout << *itr << endl;
+	}
 
 	return 0;
 }
