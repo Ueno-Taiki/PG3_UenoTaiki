3fda583c5d411f803fb91c0e972e77d0f8823c7d
上野太己 <k023g0051@g.neec.ac.jp>
2025-01-14 13:12:15 +0900

    02_04_確認課題

--- main.cpp
+++ main.cpp
@@ -1,15 +1,58 @@
 #include <stdio.h>
-#include "IShape.h"
 
-int main() {
-	IShape* iShapes[3];
+class IShape
+{
+public:
+	virtual void Size() = 0;
+	virtual void Draw() = 0;
 
-	for (int i = 0; i < 2; i++) {
-		if (i < 1) 
-			iShapes[i] = new Circle;
-		else 
-			iShapes[i] = new Rectangle;
-	}
+protected:
+	float size;
+	float radius;
+};
+
+class Circle :public IShape
+{
+public:
+	void Size();
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+class Rectangle :public IShape
+{
+public:
+	void Size() override;
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+void IShape::Size() {}
+
+void Circle::Size()
+{
+	radius = 5.0f;
+	printf("円の半径 %f\n", radius);
+	size = radius * radius * 3.14f;
+}
+
+void Rectangle::Size()
+{
+	radius = 5.0f;
+	printf("矩形の半径 %f\n", radius);
+	size = radius * 2.0f * radius * 2.0f;
+}
+
+int main(void) {
+
+	IShape *ishape[2] = { new Circle ,new Rectangle };
+
+	ishape[0]->Size();
+	ishape[1]->Size();
+
+	ishape[0]->Draw();
+	ishape[1]->Draw();
+
+	delete ishape[0];
+	delete ishape[1];
 
 	return 0;
 }
